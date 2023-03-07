e2fb28740ca811f4c7cf394cc5943f594b918286
youngmanpark <py980627@skuniv.ac.kr>
2023-03-07 22:39:45 +0900

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
         System.out.println("this is test" );
+        System.out.println("this is test 2");
     }
 }
